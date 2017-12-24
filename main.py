cce0acf330cbc5c82543d2ffe6f5d38594f03f1a
Chandler Lofland <chandlerl14@outlook.com>
2017-12-24 16:04:36 -0500

    Added mysql test file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 import couchdb
 from json import loads
 app = Flask(__name__)
-db_server = ''
+db_server = '' # change this to mysql
 with open('server0.json') as f:
     db_server = loads(f.read())
 couch = couchdb.Server('http://{}:5984/'.format(db_server))
---
+++ sqlclienttest.py
@@ -0,0 +1,32 @@
+import MySQLdb as db
+from json import loads
+
+# based on https://gist.github.com/kirang89/7161185
+
+HOST = ''
+PASSWORD = ''
+PORT = 3306
+USER = ''
+DB = ''
+url_id = 'XD1'
+
+with open('server0.json') as serverfile:
+    data = loads(serverfile)
+    HOST = data['main']
+    PASSWORD = data['pass']
+    USER = data['user']
+    DB = data['db']
+
+try:
+    connection = db.Connection(host=HOST,port=PORT,user=USER,passwd=PASSWORD,db=DB)
+
+    dbhandler = connection.cursor()
+    dbhandler.execute("SELECT * FROM {}".format(url_id))
+    result = dbhandler.fetchall()
+    for item in result:
+        print(item)
+except Exception as e:
+    print(e)
+
+finally:
+    connection.close()
