d3cf37a1bf6a2d76f9d85a422879acdd4ea14a4e
chand1012 <chandlerl14@outlook.com>
2018-01-10 23:05:07 -0500

    Added wsgi script

---
+++ wsgi.py
@@ -0,0 +1,5 @@
+# https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-uwsgi-and-nginx-on-ubuntu-14-04
+from mmain import application
+
+if __name__ == "__main__":
+    application.run()
